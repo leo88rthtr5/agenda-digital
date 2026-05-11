0e2556b958ce25571234f3046641a5912695a8ff
leo88rthtr5 <leonel.g.murrieta@gmail.com>
2026-05-10 22:54:50 -0600

    Modify CONFIG with new phone number and password
    Updated the phone number and added admin password.

--- agenda-digital/config.js
+++ agenda-digital/config.js
@@ -1,8 +1,9 @@
 const CONFIG = {
   APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbyXXnINzldWyPREEMD_cFvdw2jPPxdEjUTts2HanKoarO3LGqgfbQiITDZAeLPZva8R/exec',
-  PHONE: '5212381234567',
+  PHONE: '522225148405',
   BUSINESS: 'Peluquería de Prueba',
   DAYS_AHEAD: 7,
   DEFAULT_SLOTS: ['09:00','10:00','11:00','12:00','14:00','15:00','16:00','17:00'],
-  TIMEZONE: 'America/Mexico_City'
+  TIMEZONE: 'America/Mexico_City',
+  ADMIN_PASSWORD: 'peluqueria2024'
 };
