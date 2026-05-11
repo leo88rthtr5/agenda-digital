19e57aa550fe4cd320e14cc1173377851a6cd2be
leo88rthtr5 <leonel.g.murrieta@gmail.com>
2026-05-10 23:47:08 -0600

    Update configuration values in config.js

--- agenda-digital/config.js
+++ agenda-digital/config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbyXXnINzldWyPREEMD_cFvdw2jPPxdEjUTts2HanKoarO3LGqgfbQiITDZAeLPZva8R/exec',
-  PHONE: '522225148405',
+  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbyXXnINzldWyPREEWD_cFvdw2jPPxdEjUTts2HanKoar03LGqgfBQiITDZAeLPZva8R/exec',
+  PHONE: '5212381234567',
   BUSINESS: 'Peluquería de Prueba',
   DAYS_AHEAD: 7,
   DEFAULT_SLOTS: ['09:00','10:00','11:00','12:00','14:00','15:00','16:00','17:00'],
